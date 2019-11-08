5bb46f50b496b3076296a30d213220a7849b0ece
Sanjana P <sanjana.awsdevops@gmail.com>
2019-11-08 14:42:07 -0600

    Edited VPC variable.tf file.

--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,37 +1,37 @@
 # Global Variables ---------------------------
 variable "region" {
   type        = "string"
-  description = "region"
+  description = "Enter the region id where the VPC resides."
 }
 
 variable "profile" {
   type        = "string"
-  description = "user profile"
+  description = "Enter the AWS profile name specific to the account being used."
 }
 
 # VPC Variables ------------------------------
 variable "vpc_name" {
   type        = "string"
-  description = "name of vpc"
+  description = "Enter the name of a VPC."
 }
 
 variable "vpc_cidr_block" {
   type        = "string"
-  description = "cider block for vpc"
+  description = "Enter the cider block of the VPC."
 }
 
 # Subnet Variables --------------------------
 variable "availability_zone_names" {
   type        = "list"
-  description = "az"
+  description = "Enter all the respective availability zones."
 }
 
 variable "public_cidr" {
   type        = "list"
-  description = "public cider"
+  description = "Enter all the respective public cidrs."
 }
 
 variable "private_cidr" {
   type        = "list"
-  description = "public cider"
+  description = "Enter all the respective private cidrs."
 }
